e4a8f46589df42add1a264b79433d1a4dce95224
Jashwanthkashaboina <jashwanthkashaboina02@gmail.com>
2025-10-13 08:35:58 +0530

    Added new and create route

--- app.js
+++ app.js
@@ -29,12 +29,25 @@ app.get("/listings",async(req,res)=>{
     res.render("listings/index",{allListings});
 });
 
+// New Route -- To create a new Listing
+app.get("/listings/new",(req,res)=>{
+    res.render("listings/new.ejs");
+});
+
 //show route
 app.get("/listings/:id",async(req,res)=>{
     let {id} = req.params;
     const listing = await Listing.findById(id);
     res.render("listings/show.ejs",{ listing });
 });
+
+//Create Route
+app.post("/listings",async(req,res)=>{
+    const newListing = new Listing(req.body.listing);
+    await newListing.save();
+    res.redirect("/listings");
+});
+
 // app.get("/testListing",async (req,res)=>{
 //     let sampleListing = new Listing({
 //         title: "My new Villa",
