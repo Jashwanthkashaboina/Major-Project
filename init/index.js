c7c7c9a0589a6a6a67d13df69a78ff0c49ac2e88
Jashwanthkashaboina <jashwanthkashaboina02@gmail.com>
2025-10-14 06:21:51 +0530

    Modified index.js

--- init/index.js
+++ init/index.js
@@ -10,7 +10,7 @@ const mongoose = require("mongoose");
          console.log(err);
      });
  async function main(){
-     await mongoose.connect("mongodb://127.0.0.1:27017/wanderlust");
+    await mongoose.connect("mongodb://127.0.0.1:27017/wanderlust");
  }
 
 
