3c07813b9f24ab43fbee4ac71930890045f84410
Jashwanthkashaboina <jashwanthkashaboina02@gmail.com>
2025-10-14 07:56:30 +0530

    Added and Updated Files (ejs-mate) is used (boilerPlate)

--- app.js
+++ app.js
@@ -4,6 +4,8 @@ const mongoose = require("mongoose");
 const Listing = require("./models/listing.js");//to use the model we created in listin.js first we need to require 
 const path = require("path");
 const methodOverride = require("method-override");
+const ejsMate = require("ejs-mate");
+
 main()
     .then(()=>{
         console.log("connected to DB");
@@ -19,7 +21,7 @@ app.set("view engine","ejs");
 app.set("views",path.join(__dirname,"views"));
 app.use(express.urlencoded({extended: true}));
 app.use(methodOverride("_method"));
-
+app.engine("ejs",ejsMate);
 
 app.get("/",(req,res)=>{
     res.send("Hi! I'm root");
