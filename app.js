ff23fe8a1be7e5058d65e294df354d54e27b9473
Jashwanthkashaboina <jashwanthkashaboina02@gmail.com>
2025-10-17 08:37:41 +0530

    Updated

--- app.js
+++ app.js
@@ -48,11 +48,14 @@ app.get("/listings/:id",async(req,res)=>{
 });
 
 //Create Route
-app.post("/listings",async(req,res)=>{
-    const newListing = new Listing(req.body.listing);
-    await newListing.save();
-    res.redirect("/listings");
+app.post("/listings", async (req, res) => {
+  const newListing = new Listing(req.body.listing);
+  await newListing.save();
+  res.redirect("/listings");
 });
+
+
+
 //Edit Route
 app.get("/listings/:id/edit",async(req,res)=>{
     let {id} = req.params;
