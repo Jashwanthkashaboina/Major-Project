7812b26cf9e0a487225772f81a4cdfd08b8dd0ec
Jashwanthkashaboina <jashwanthkashaboina02@gmail.com>
2025-10-19 11:45:35 +0530

    Updated validation messages

--- app.js
+++ app.js
@@ -34,10 +34,10 @@ app.get("/",(req,res)=>{
 
 
 const validateListing = (req,res,next)=>{
-    let {error} = listingSchema.validate(req.body.listing);
+    let {error} = listingSchema.validate(req.body);
     if(error){
-        let errMsg = error.details.map((el)=>el.message).join(",");
-        throw new ExpressError(400,errMsg);
+        //let errMsg = error.details.map((el)=>el.message).join(",");
+        throw new ExpressError(400,error.toString());
     }
     else next();
 }
