8bc6391ba57e5e0072880540be96c2121c352cb2
Jashwanthkashaboina <jashwanthkashaboina02@gmail.com>
2025-10-23 07:56:01 +0530

    Added user route

---
+++ routes/user.js
@@ -0,0 +1,9 @@
+const express = require("express");
+const router = express.Router({ mergeParams: true });
+
+
+router.get("/signup",(req,res)=>{
+    res.send("Form !");
+});
+
+module.exports = router;
