bc86e43ff2bc42e2b603562e29ac8571eeefc261
Jashwanthkashaboina <jashwanthkashaboina02@gmail.com>
2025-10-26 06:56:24 +0530

    Add listings.js tranferred core functionality of backend -- listings

---
+++ controllers/listings.js
@@ -0,0 +1,69 @@
+//This file contains Core Functionality of Backend
+const { model } = require("mongoose");
+const Listing = require("../models/listing.js");
+
+module.exports.index = async(req,res)=>{
+    const allListings = await Listing.find({});
+    res.render("listings/index",{allListings});
+};
+
+module.exports.renderNewForm = (req,res)=>{
+    res.render("listings/new.ejs");
+};
+
+
+module.exports.showListings = async(req,res)=>{
+    let {id} = req.params;
+    //populate -- to get the details of reviews and owner instead of just their ids
+    const listing = await Listing.findById(id).populate({ path : "reviews",populate: { path: "author" } }).populate("owner");
+    if(!listing){
+        req.flash("error","Listing you requested for does not exists");
+        res.redirect("/listings");
+        return;
+    }
+    console.log(listing);
+    res.render("listings/show.ejs",{ listing });
+};
+
+
+module.exports.createListing = async(req, res) => {
+        if (!req.body.listing.image || req.body.listing.image.trim() === "") {
+            req.body.listing.image = "https://images.unsplash.com/photo-1566073771259-6a8506099945?auto=format&fit=crop&w=800&q=60";
+        }
+        const newListing = new Listing(req.body.listing);
+        //every time when we create listingg by default owner is not stored as we didnt created in schema
+        // so first we need to store the curr user information
+        //we know that passport store the user related info... in "req.user"
+        //req.user has access to many 
+        newListing.owner = req.user._id;
+        await newListing.save();
+        req.flash("success","New Listing created !");
+        res.redirect("/listings");
+
+};
+
+module.exports.renderEditForm = async(req,res)=>{
+    let {id} = req.params;
+    let listing = await Listing.findById(id);
+    if(!listing){
+        req.flash("error","Listing you requested for does not exists");
+        res.redirect("/listings");
+        return;
+    }
+    res.render("listings/edit.ejs",{listing});
+
+};
+
+module.exports.updateListing = async(req,res)=>{
+    let {id} = req.params;
+    await Listing.findByIdAndUpdate(id,{...req.body.listing});
+    req.flash("success","Listing Updated");
+    res.redirect(`/listings/${id}`);
+};
+
+module.exports.deleteListing = async(req,res)=>{
+    let {id} = req.params;
+    await Listing.findByIdAndDelete(id);
+    req.flash("success","Listing Deleted");
+    res.redirect("/listings");
+};
