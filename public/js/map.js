f130282d157d64b207982c5e97bf7d911659d301
Jashwanthkashaboina <jashwanthkashaboina02@gmail.com>
2025-10-29 07:25:28 +0530

    Added map.js

---
+++ public/js/map.js
@@ -0,0 +1,8 @@
+// <!-- Map Box Script -->
+    mapboxgl.accessToken = mapToken;
+    const map = new mapboxgl.Map({
+        container: 'map', // container ID
+        style: 'mapbox://styles/mapbox/streets-v12',
+        center: [74.8723,31.6340], // starting position [lng, lat]. Note that lat must be set between -90 and 90
+        zoom: 9 // starting zoom
+    });
