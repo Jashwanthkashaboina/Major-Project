7764d9820f98aee66c01b082c8ff37c7dc144c2e
Jashwanthkashaboina <jashwanthkashaboina02@gmail.com>
2025-10-30 22:45:23 +0530

    connect render to github

--- app.js
+++ app.js
@@ -10,6 +10,7 @@ const methodOverride = require("method-override");
 const ejsMate = require("ejs-mate");
 const ExpressError = require("./utils/ExpressError.js");
 const session = require("express-session");
+const MongoStore = require('connect-mongo');
 const flash = require("connect-flash");
 const passport = require("passport");
 const LocalStrategy = require("passport-local");
@@ -23,8 +24,20 @@ const listingRouter = require("./routes/listing.js");
 const reviewRouter = require("./routes/review.js");
 const userRouter = require("./routes/user.js");
 
+
+const store = MongoStore.create({
+    mongoUrl: dbUrl,
+    crypto:{
+        secret: process.env.SECRET,
+    },
+    touchAfter: 24 * 3600,
+});
+store.on("error",(err)=>{
+    console.log("ERROR OCCURED IN MONGO STORE",err);
+});
 const sessionOptions = {
-    secret: "mysupersecret",
+    store: store,
+    secret: process.env.SECRET,
     resave: false,
     saveUninitialized: true,
     cookie: {
@@ -42,6 +55,9 @@ main()
 }).catch((err)=>{
     console.log(err);
 });
+
+// console.log("Connecting to : ",process.env.ATLASDB_URL);
+
 async function main(){
     await mongoose.connect(dbUrl);
 }
@@ -58,7 +74,6 @@ app.use(express.static(path.join(__dirname,"/public")));
 // app.get("/",(req,res)=>{
 //     res.send("Hi! I'm root");
 // });
-
 app.use(session(sessionOptions));
 app.use(flash());
 
